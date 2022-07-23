38ac4f2b98b46806cd42801d0cf824f92b5e5b1e
SAKSHAM SINGH <sakshamsikarwar01@gmail.com>
2022-07-24 00:00:17 +0530

    small iteration on for less memory usage

--- StringValue_problem.py
+++ StringValue_problem.py
@@ -13,16 +13,16 @@ for i in range(len(valueList)):
                 dict1[aList[i]].append(valueList[i])
             else:
                 dict1[aList[i]].append(valueList[i])
-                
+
+dict2.update(dict1)                
         
-for i in range(len(dict1)):
-    if len(dict1[aList[i]]) > 1:
-        dict2[aList[i]] = dict1[aList[i]]
+for i in dict1:
+    if len(dict1[i]) == 1:
+        dict2.pop(i)
         
 sum = 0
 
 for i in dict2:
     sum += min(dict2[i])
 
-print(dict2)
 print(sum)
